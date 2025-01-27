d700385a3f34dcb235693db659912d0a56333790
naimish-rathod <nr553451@gmail.com>
2025-01-27 19:30:32 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>turboCar</title>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- Main Css -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- Fonts -->
+	<link rel="stylesheet" type="text/css" href="css/font.css">
+	<!-- Responsive Css -->
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+</head>
+<body>
+<!-- Header -->
+<section>
+	<nav class="navbar navbar-expand-md"> 
+		<div class="container-fluid my-navbar">
+			<!-- logo icon -->
+			<a href="" class="navbar-brand">
+				<img src="img/logo-7.svg">
+			</a>
+			<!-- Hamburg icon -->
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+	      		<span class="navbar-toggler-icon"></span>
+	    	</button>
+			<div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a href="" class="nav-link">Shop</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Sell</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Trade</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">More</a>
+					</li>
+					<li class="nav-item ps-5">
+						<button class="my-btn">Sign Up</button>
+					</li>
+				</ul>				
+			</div>
+		</div>
+	</nav>
+</section>
---
+++ index.php
@@ -0,0 +1,163 @@
+<?php 
+include 'header.php';
+?>
+														<!-- Baner section -->
+<section>
+	<div class="my-container">
+		<div class="search-div">
+			<div class="search-first">
+				<div class="search-txt">
+					<p>MAKE</p>
+					<p>Type the car make</p>
+				</div>
+				<div class="search-txt">
+					<p>MODEL</p>
+					<p>Add the car model</p>
+				</div>
+				<div class="search-txt">
+					<p>MILES</p>
+					<p>Type miles</p>
+				</div>
+			</div>
+			<div class="search-btn-box">
+				<button class="my-btn search-btn"><img src="img/search.png">Search</button>
+				<button class="ai-btn">Search with AI</button>
+			</div>
+		</div>
+		<div class="big-txt">
+			<div class="rating-box">
+				<div><img src="img/5star.png"></div>
+				<div><p>4.8 (4500 reviews)</p></div>
+			</div>
+			<p class="car-buy-txt">The Way Car<br>Buying Should Be</p>
+			<p class="carmax-txt">Upfront prices.CarMax certified quality.</p>
+		</div> 
+		<div class="card-box">
+			<div>
+				<p class="reason-txt">Reasons Why You Should Buy on TurboCar</p>
+			</div>
+			<div class="card-wrapper">
+				<div class="bnr-card card-1">
+					<div>
+						<p>Try it for 7 days or 300km</p>
+						<p>You dont't like it? dont worry change it for another car</p>
+					</div>
+				</div>
+				<div class="bnr-card card-2">
+					<div>
+						<p>Try it for 7 days or 300km</p>
+						<p>You dont't like it? dont worry change it for another car</p>
+					</div>
+				</div>
+				<div class="bnr-card card-3">
+					<div>
+						<p>Try it for 7 days or 300km</p>
+						<p>You dont't like it? dont worry change it for another car</p>
+					</div>
+				</div>
+				<div class="bnr-card card-4">
+					<div>
+						<p>Try it for 7 days or 300km</p>
+						<p>You dont't like it? dont worry change it for another car</p>
+					</div>
+				</div>
+				<div class="bnr-card card-5">
+					<!-- Only image -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+														<!-- Pre-Qualify section -->
+<section>
+	<div class="my-container">
+		<div class="price">
+			<p class="price-box-txt">Pre-Qualify With No impact To Your Credit</p>
+			<div class="price-box">
+				<div class="price-box-1">
+					<div><p class="kind-txt">What kinds of cars do you want ?</p></div>
+					<div class="cars-list">
+						<div class="cars-blk">
+							<div><img src="img/suv.svg"></div>
+							<div><p>SUV's</p></div>
+						</div>
+						<div class="cars">
+							<div><img src="img/coupe.svg"></div>
+							<div><p>Coupe</p></div>
+						</div>
+						<div class="cars">
+							<div><img src="img/coupe.svg"></div>
+							<div><p>Hatchback</p></div>
+						</div>
+						<div class="cars">
+							<div><img src="img/coupe.svg"></div>
+							<div><p>MPV</p></div>
+						</div>
+						<div class="cars">
+							<div><img src="img/coupe.svg"></div>
+							<div><p>Hybrids</p></div>
+						</div>
+						<div class="cars">
+							<div><img src="img/coupe.svg"></div>
+							<div><p>Electric</p></div>
+						</div>
+						<div class="cars">
+							<img src="img/rightbtn.png">
+						</div>
+					</div>
+					<div class="payment-mode">
+						<div class="payment">
+							<p>Monthly Payment</p>
+							<p>$400</p>
+						</div>
+						<div class="payment">
+							<p>Down Payment</p>
+							<p>$2,500</p>
+						</div>
+						<div class="payment">
+							<p>Credit Score</p>
+							<p>Good (670-730)▼</p>
+						</div>
+					</div>
+				</div>
+				<div class="price-box-2">
+					<div><p>Est. Vehicle Price <img src="img/iicon.png"></p></div>
+					<p class="dol-num">$20,702</p>
+					<div><button class="my-btn">Get Pre-Qualified</button></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+													<!-- blog section -->
+<section>
+	<div class="my-container">
+		<div class="blog-heading">
+			<div class="heading-txt">
+					<p>Suggested For You</p>
+					<p>Based on recently viewed vehicles</p>
+				</div>
+			<div class="blog-btn">
+				<button class="btn p-0" id="prevarr"><img src="img/leftbtn.png"></button>
+				<button class="btn p-0" id="nextarr"><img src="img/rightbtnb.png"></button>
+			</div>
+		</div>
+		<div>
+			<div class="blog-card">
+				<div><img src="img/tesla.png" class="img-fluid"></div>
+				<div class="blg-all-txt">
+					<div class="blg-txt-1"> 
+						<p>Tesla Model 3 2024</p> 
+						<p>★4.97</p>
+					</div>
+					<div class="blg-txt-2"><p>Electric 6K miles</p></div>
+					<div><p>Free shipping</p> <p>CarMax Dulles, VA</p></div>
+					<div>
+						<p>$84,998</p>
+						<p>Estimated $562/mo</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
